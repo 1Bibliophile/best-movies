ebaf5a6371f2d1ef831d6e91dfcc554712773484
David Morgenstern <DavidtheConfident@gmail.com>
2022-01-10 15:36:55 -0600

    Added movie rated to db.json

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,7 +1,9 @@
 export const MovieCard = ({movie}) => {
+    
     return (
         <div className="movie-card">
-            <h3>{movie.name}</h3>
+            <h3>{`${movie.name} (${movie.date})`}</h3>
+            <h5>{`Rating: ${movie.rating}`}</h5>
             <img src={movie.image} alt={`${movie.name} from ${movie.date}`}/>
         </div>
     )
