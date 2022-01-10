0e2ca98152546ab7c4dd1cd162dfeebea755a59a
David Morgenstern <DavidtheConfident@gmail.com>
2022-01-10 15:41:14 -0600

    Increased font size of rating & rated

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -3,7 +3,7 @@ export const MovieCard = ({movie}) => {
     return (
         <div className="movie-card">
             <h3>{`${movie.name} (${movie.date})`}</h3>
-            <h5>{`Rating: ${movie.rating}`}</h5>
+            <h4>{`Rated: ${movie.rated} - Rating: ${movie.rating}`}</h4>
             <img src={movie.image} alt={`${movie.name} from ${movie.date}`}/>
         </div>
     )
