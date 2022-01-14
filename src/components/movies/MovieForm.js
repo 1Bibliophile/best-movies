0da913193c58cebfe87458bdeb74f16f25aafec3
David Morgenstern <DavidtheConfident@gmail.com>
2022-01-13 22:15:20 -0600

    onChange for New Movies

--- src/components/movies/MovieForm.js
+++ src/components/movies/MovieForm.js
@@ -1,21 +1,44 @@
+import { useState } from "react"
+
 export const MovieForm = () => {
+    
+    const [name, setName] = useState("")
+    const [date, setDate] = useState("")
+    const [image, setImage] = useState("")
+    const [rated, setRated] = useState("")
+    const [rating, setRating] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+
+    }
+    
+    
     return (
         <div>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
+            <form className="movie-form"
+             onSubmit={handleSubmit}>
+                <label htmlFor="name">Name: </label>
+                <input onChange={e => setName(e.target.value)} 
+                 type="text" 
+                name="name" id="name" value={name}/><br />
 
-                <label htmlFor="date">Date</label>
-                <input type="text" name="date" id="date" />
+                <label htmlFor="date">Date: </label>
+                <input onChange={e => setDate(e.target.value)} type="text" 
+                name="date" id="date" value={date}/><br />
 
-                <label htmlFor="image">Image</label>
-                <input type="text" name="image" id="image" />
+                <label htmlFor="image">Image: </label>
+                <input onChange={e => setImage(e.target.value)}type="text" 
+                name="image" id="image" value={image}/><br />
 
-                <label htmlFor="mpaa">MPAA Rating</label>
-                <input type="text" name="rated" id="rated" />
+                <label htmlFor="mpaa">MPAA Rating: </label>
+                <input onChange={e => setRated(e.target.value)} type="text" 
+                name="rated" id="rated" value={rated}/><br />
 
-                <label htmlFor="rating">Movie Rating</label>
-                <input type="text" name="rating" id="rating" />
+                <label htmlFor="rating">Movie Rating: </label>
+                <input onChange={e => setRating(e.target.value)} type="text" 
+                name="rating" id="rating" value={rating}/><br />
             </form>
         </div>
     )
