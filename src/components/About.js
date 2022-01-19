a39a63cb9ae4c1dca6f32a031fc0284f98daec50
David Morgenstern <DavidtheConfident@gmail.com>
2022-01-18 22:09:11 -0600

    Roughdraft for About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,13 @@
 export const About = () => {
     return (
         <div>
-            
+            <br />
+            <h2>
+            Click here to view GitHub: <a href="https://github.com/1Bibliophile/best-movies">GitHub</a>
+            <br /><br />
+            Click here to view Blog: <a href="https://medium.com/p/5525b22785e6/edit">Blog</a>
+                
+            </h2>
         </div>
     )
 }
